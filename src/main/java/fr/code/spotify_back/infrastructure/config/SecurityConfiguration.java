12ff6b0b22cec714331e04328115ec9da241962a
unknown <deharo.florian@gmail.com>
2024-12-23 22:35:29 +0100

    fix the authentification part

--- src/main/java/fr/code/spotify_back/infrastructure/config/SecurityConfiguration.java
+++ src/main/java/fr/code/spotify_back/infrastructure/config/SecurityConfiguration.java
@@ -18,30 +18,18 @@ import org.springframework.web.cors.CorsConfigurationSource;
 @Configuration
 public class SecurityConfiguration {
 	
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source ; 
-	}
-	
-	@Bean
-	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(authorize -> authorize
-				.requestMatchers(HttpMethod.GET, "api/songs").permitAll()
-                .requestMatchers("/login/**", "/oauth2/**").permitAll() 
-				.requestMatchers(HttpMethod.GET, "api/song/search").permitAll()
-				.anyRequest().authenticated())	
-				.cors(c -> c.configurationSource(corsConfigurationSource()))
-				.csrf(csrf ->
-						csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-						.csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()))
-				.oauth2Login(Customizer.withDefaults())
-				.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-				.oauth2Client(Customizer.withDefaults());
-		return http.build();
-	}
+	   @Bean
+	    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+	        http.authorizeHttpRequests(authorize -> authorize
+	                .requestMatchers(HttpMethod.GET, "api/songs").permitAll()
+	                .requestMatchers(HttpMethod.GET, "api/songs/search").permitAll()
+	                .anyRequest().authenticated())
+	                .csrf(csrf ->
+	                        csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+	                        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()))
+	                .oauth2Login(Customizer.withDefaults())
+	                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+	                .oauth2Client(Customizer.withDefaults());
+	        return http.build();
+	    }
 }
--- src/main/java/fr/code/spotify_back/usercontext/presentation/AuthResource.java
+++ src/main/java/fr/code/spotify_back/usercontext/presentation/AuthResource.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,43 +21,34 @@ import fr.code.spotify_back.usercontext.ReadUserDTO;
 import fr.code.spotify_back.usercontext.application.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 
+
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api")
 public class AuthResource {
-	
-	private final UserService userService;
-	
-	private final ClientRegistration registration;
-	
-	public AuthResource(UserService userService, ClientRegistrationRepository registrations) {
-		this.userService = userService;
-		this.registration = registrations.findByRegistrationId("okta") ;
-	}
-	
-	@GetMapping("/get-authenticated-user")
-	public ResponseEntity<ReadUserDTO> getAuthenticatedUser(@AuthenticationPrincipal OAuth2User user){
-			System.out.print("erreur");
-		if ( user == null) {
-			return new ResponseEntity<>( HttpStatus.INTERNAL_SERVER_ERROR);
-		}else {
-			userService.syncWithIdp(user);
-			ReadUserDTO userFromAuthentication = userService.getAuthenticatedUserFromSecurityContext();
-			return ResponseEntity.ok().body(userFromAuthentication);
-		}
-		
-	}
-	
-	
-	
-	@PostMapping("/logout")
-	public ResponseEntity<?> logout(HttpServletRequest request) {
-		String issuerUri = registration.getProviderDetails().getIssuerUri();
-		String originUrl = request.getHeader(HttpHeaders.ORIGIN);
-		Object[] params = {issuerUri, registration.getClientId(), originUrl};
-		String logoutUrl = MessageFormat.format("{0}v2/logout?clientId={1}&returnTo={2}", params);
-		request.getSession().invalidate();
-		return ResponseEntity.ok().body(Map.of("logoutUrl", logoutUrl));
-	}
-	
-
+    private final UserService userService;
+    private final ClientRegistration registration;
+    public AuthResource(UserService userService, ClientRegistrationRepository registrations) {
+        this.userService = userService;
+        this.registration = registrations.findByRegistrationId("okta");
+    }
+    @GetMapping("/get-authenticated-user")
+    public ResponseEntity<ReadUserDTO> getAuthenticatedUser(@AuthenticationPrincipal OAuth2User user) {
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        } else {
+            userService.syncWithIdp(user);
+            ReadUserDTO userFromAuthentication = userService.getAuthenticatedUserFromSecurityContext();
+            return ResponseEntity.ok().body(userFromAuthentication);
+        }
+    }
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request) {
+        String issuerUri = registration.getProviderDetails().getIssuerUri();
+        String originUrl = request.getHeader(HttpHeaders.ORIGIN);
+        Object[] params = {issuerUri, registration.getClientId(), originUrl};
+        String logoutUrl = MessageFormat.format("{0}v2/logout?client_id={1}&returnTo={2}", params);
+        request.getSession().invalidate();
+        return ResponseEntity.ok().body(Map.of("logoutUrl", logoutUrl));
+    }
 }
